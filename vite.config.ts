8e6d338b7ef2d519ecd67ce1bebc9d0d0fb9d26b
Eivind Melleby <emelleby@gmail.com>
2026-04-05 14:18:21 +0200

    Deployment to Cloudflare

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,44 @@
-import { defineConfig } from 'vite'
-import { devtools } from '@tanstack/devtools-vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
+import contentCollections from '@content-collections/vite'
 import { paraglideVitePlugin } from '@inlang/paraglide-js'
+import tailwindcss from '@tailwindcss/vite'
+import { devtools } from '@tanstack/devtools-vite'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
-import viteTsConfigPaths from 'vite-tsconfig-paths'
-import { fileURLToPath, URL } from 'url'
-
-import tailwindcss from '@tailwindcss/vite'
-import contentCollections from '@content-collections/vite'
 import { nitro } from 'nitro/vite'
+import { fileURLToPath, URL } from 'url'
+import { defineConfig } from 'vite'
+import viteTsConfigPaths from 'vite-tsconfig-paths'
 
 const config = defineConfig({
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-    },
-  },
-  plugins: [
-    devtools(),
-    paraglideVitePlugin({
-      project: './project.inlang',
-      outdir: './src/paraglide',
-      strategy: ['url'],
-    }),
-    contentCollections(),
-    nitro(),
-    // this is the plugin that enables path aliases
-    viteTsConfigPaths({
-      projects: ['./tsconfig.json'],
-    }),
-    tailwindcss(),
-    tanstackStart({ preset: 'node-ws' }),
-    viteReact({
-      babel: {
-        plugins: ['babel-plugin-react-compiler'],
-      },
-    }),
-  ],
+	resolve: {
+		alias: {
+			'@': fileURLToPath(new URL('./src', import.meta.url)),
+		},
+	},
+	plugins: [
+		devtools(),
+		paraglideVitePlugin({
+			project: './project.inlang',
+			outdir: './src/paraglide',
+			strategy: ['url'],
+		}),
+		contentCollections(),
+		nitro(),
+		cloudflare({ viteEnvironment: { name: 'ssr' } }),
+		// this is the plugin that enables path aliases
+		viteTsConfigPaths({
+			projects: ['./tsconfig.json'],
+		}),
+		tailwindcss(),
+		tanstackStart({ preset: 'node-ws' }),
+		// tanstackStart({ preset: 'cloudflare-pages' }),
+		viteReact({
+			babel: {
+				plugins: ['babel-plugin-react-compiler'],
+			},
+		}),
+	],
 })
 
 export default config
