dc733b1613d09e745f6d16315654d8094d5881bf
Eivind Melleby <emelleby@gmail.com>
2026-04-06 15:05:32 +0200

    Is it the Publishable?

--- src/integrations/clerk/provider.tsx
+++ src/integrations/clerk/provider.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react'
 
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 if (!PUBLISHABLE_KEY) {
-	throw new Error('Add your Clerk Publishable Key to the .env.local file')
+	throw new Error('Add your Clerk Publishable Key to the .env.local file!')
 }
 
 export default function AppClerkProvider({
